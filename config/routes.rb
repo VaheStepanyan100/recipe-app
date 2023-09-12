5a516ab62da451050b489129be62fb03e3cbff36
Junaid Syed <sjunaid626@gmail.com>
2023-09-12 17:43:52 +0530

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
     root 'recipes#index', as: :authenticated_root
   end
 
-  root to: 'welcome#index'
-
   resources :recipes do
     member do
       delete :destroy
