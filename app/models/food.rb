bde18f4b453410165a9739eb051a3fcf24ed35c7
VaheStepanyan100 <vahestepanyan100@gmail.com>
2023-09-12 15:56:07 +0400

    Update Models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,8 @@
 class Food < ApplicationRecord
-  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'food_id'
-  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :user
+  has_many :recipe_foods
+  has_many :recipes, through: :recipe_foods
 
-  validates :name, presence: true, length: { in: 2..250 }
-  validates :measurement_unit, presence: true
-  validates :price, presence: true, numericality: { only_float: true, greater_than_or_equal_to: 0 }
-  validates :quantity, presence: true
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
