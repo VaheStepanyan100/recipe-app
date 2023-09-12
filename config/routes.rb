b0f18e27ed4937ca2d571c851dd42b3da5cf5d29
Junaid Syed <sjunaid626@gmail.com>
2023-09-12 17:27:44 +0530

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
 
-  root "recipes#index"
+  authenticated :user do
+    root 'recipes#index', as: :authenticated_root
+  end
+
+  root to: 'welcome#index'
+
+  resources :recipes do
+    member do
+      delete :destroy
+    end
+  end
 end
