c844a765ad55152e51d0c387c9b998fc9e9b1990
Junaid Syed <sjunaid626@gmail.com>
2023-09-12 17:26:53 +0530

    Add dependant

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id'
-
+  has_many :recipe_foods, class_name: 'RecipeFood', foreign_key: 'recipe_id', dependent: :destroy
+  
   validates :name, presence: true, length: { in: 2..250 }
   validates :description, presence: true
   validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
