b0792ca94ee504357c5ab347f7264257b3c5c49a
Junaid Syed <sjunaid626@gmail.com>
2023-09-13 13:12:50 +0530

    Update the routes for recipes and for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
 
   resources :foods, only: [:index, :show, :new, :create, :destroy]
-
-  root "foods#index"
+  resources :recipes, only: [:index, :show, :new, :create, :destroy]
+  
+  root "welcome#index"
 end
